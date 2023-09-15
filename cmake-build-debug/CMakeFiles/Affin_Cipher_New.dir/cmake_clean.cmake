033169a560225dc563f73c1b3baaee7a2365eacf
Carmonel <zhurkovjaroslav@gmail.com>
2023-09-15 23:50:08 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/Affin_Cipher_New.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Affin_Cipher_New.exe"
+  "Affin_Cipher_New.pdb"
+  "CMakeFiles/Affin_Cipher_New.dir/main.cpp.o"
+  "libAffin_Cipher_New.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Affin_Cipher_New.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
